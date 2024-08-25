281988162e9d144e19886c428270a427dbdfb466
Sumit <sverma162003@gmail.com>
2024-08-25 21:47:27 +0530

    "ADDED CODE TO LOGIN PAGE,FOOTER,HEADER,ABOUT AND LOGIN CSS"

---
+++ src/app/pages/about.js
@@ -0,0 +1,9 @@
+const About = () => (
+    <div>
+      <h1>About Us</h1>
+      <p>This is a website for finding rental properties.</p>
+    </div>
+  )
+  
+  export default About
+  
---
+++ src/app/utils/fetcher.js
@@ -0,0 +1,8 @@
+export const fetcher = async (url) => {
+    const response = await fetch(url)
+    if (!response.ok) {
+      throw new Error('Failed to fetch')
+    }
+    return response.json()
+  }
+  
